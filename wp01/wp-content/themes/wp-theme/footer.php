f9af2fef54985bc6994fc11b5e0e4748e3766ebc
iwaki1990 <low.son.wing@gmail.com>
2021-05-31 15:33:42 +0900

    1 commit

---
+++ wp01/wp-content/themes/wp-theme/footer.php
@@ -0,0 +1,32 @@
+<footer id="footer">
+  <div class="footer-nav__title">
+    <a href="" class="article-logo">
+      estra magazine
+      <br />
+      <span class="magazine-subtitle">
+        学習方法を提案するメディア
+      </span>
+    </a>
+  </div>
+  <div class="footer-sns flex">
+    <a href="" class="sns-icon first"><i class="fab fa-twitter"></i></a>
+    <a href="" class="sns-icon"><i class="fab fa-facebook"></i></a>
+    <a href="" class="sns-icon"><i class="fab fa-instagram"></i></a>
+  </div>
+  <div class="footer-content flex">
+    <div class="footer-about">
+      <h3>About</h3>
+      <ul>
+        <li>
+          <a href="">お問い合わせ</a>
+        </li>
+        <li><a href="https://estra.jp">運営会社</a></li>
+        <li><a href="https://coachtech.site">COACHTECH</a></li>
+      </ul>
+    </div>
+  </div>
+  <small>Copyright estra magazine 2020 All Rights Reserved.</small>
+</footer>
+</body>
+
+</html>
---
+++ wp01/wp-content/themes/wp-theme/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>estra magazine</title>
+  <link rel="stylesheet" href="css/reset.css" />
+  <link rel="stylesheet" href="css/style.css" />
+  <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+</head>
+
+<body>
+  <header class="article-nav">
+    <a href="" class="article-logo">
+      estra magazine
+      <br />
+      <span class="magazine-subtitle">
+        学習方法を提案するメディア
+      </span>
+    </a>
+  </header>
